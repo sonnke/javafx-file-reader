ef404be55cf19537b6ae298e5aad4d47cb6a1143
nonny <ns.gumede2@gmail.com>
2020-01-26 02:31:25 +0200

    display text file data

--- src/com/main/view/MainUi.java
+++ src/com/main/view/MainUi.java
@@ -15,20 +15,27 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import com.main.controller.*;
 public class MainUi extends Application{
+	
 	TextField addInput = new TextField();
 	//Initiate Class
 	MainCtr ctr = new MainCtr("streamFile.txt");
+	ObservableList<String> listValues;
+	
+	public MainUi(){
+		//Obervable list
+		listValues = FXCollections.observableArrayList(ctr.readFile());
+	}
 	
 
 	@Override
-	public void start(Stage stage) throws Exception {
+	public void start(Stage stage){
+		
 		
 		
 		//Layout
 		GridPane MainLayout = new GridPane();
 		
-		//Obervable list
-		ObservableList<String> listValues = FXCollections.observableArrayList("Sonke", "Vusi", "Zama","Thuli");
+		
 		
 		//Filter input
 		TextField filterInput = new TextField();
@@ -57,11 +64,19 @@ public class MainUi extends Application{
 		
 		GridPane.setColumnSpan(listView, 2);
 		
-		Scene MainScene = new Scene(MainLayout);
-		stage.setScene(MainScene);
-		stage.setTitle("Exam Project");
 		
-		stage.show();
+		
+		try {
+			
+			Scene MainScene = new Scene(MainLayout);
+			stage.setScene(MainScene);
+			stage.setTitle("Exam Project");
+				
+			stage.show();
+			
+		}catch(Exception e){
+			System.out.print(e.getMessage());
+		}
 		
 	}
 	
@@ -70,19 +85,23 @@ public class MainUi extends Application{
 	EventHandler<ActionEvent> addButtonEvent = new EventHandler<ActionEvent>() {
 
 		@Override
-		public void handle(ActionEvent event) {
+		public void handle(ActionEvent event){
 			// TODO Auto-generated method stub			
 			String text = addInput.getText();
-			ctr.addUser(text);
+			Boolean status = ctr.addUser(text);		
+			if(status) addInput.clear();
+			
+			listValues.setAll(FXCollections.observableArrayList(ctr.readFile()));
+			
+			
 		}
 		
 	};
 	
 	
-	 
-	
 	
 	public static void main(String[] args){
+		
 		
 		MainUi.launch(args);
 	}
