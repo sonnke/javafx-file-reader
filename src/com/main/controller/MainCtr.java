5e36f6cea74b86ea21c2a4a0fe2968c83760e174
nonny <ns.gumede2@gmail.com>
2020-01-26 02:30:07 +0200

    read text file

--- src/com/main/controller/MainCtr.java
+++ src/com/main/controller/MainCtr.java
@@ -1,5 +1,10 @@
 package com.main.controller;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class MainCtr {
 	private String fileName = null;
 	public MainCtr(String filename) {
@@ -9,20 +14,37 @@ public class MainCtr {
 	public Boolean addUser(String user) {
 		try{
 			
-			PrintWriter outputStream = new PrintWriter(this.fileName);
+			PrintWriter outputStream = new PrintWriter( new FileOutputStream(this.fileName,true));
 			//Write to a file
 			outputStream.println(user);
 			outputStream.close();
 			return true;
-		}catch(Exception e){
+		}catch(FileNotFoundException e){
 			System.out.println(e.getMessage());
 			return false;
 		}
 	}
 	
 	//Let's read file
-	public String[] readFile(){
-		return null;
+	public ArrayList<String> readFile(){
+		
+		ArrayList<String> lines = new ArrayList<String>();
+	
+		try {
+			
+			Scanner outPutStream = new Scanner(new File(this.fileName));
+			while(outPutStream.hasNextLine()){
+				lines.add(outPutStream.nextLine());
+			}
+			
+			//Close stream
+			outPutStream.close();
+			return lines;
+			
+		}catch(FileNotFoundException e){
+			System.out.println(e.getMessage());
+			return lines;
+		}
 	}
 
 }
